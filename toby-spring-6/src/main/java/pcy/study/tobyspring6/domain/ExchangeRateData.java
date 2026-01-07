bfe85c30a3663d683db775c8745ee8d88fad365a
cyPark95 <parkcy1007@gmail.com>
2026-01-07 17:43:11 +0900

    feat: 스프링6 강의 섹션 2.PaymentService 구현

---
+++ toby-spring-6/src/main/java/pcy/study/tobyspring6/domain/ExchangeRateData.java
@@ -0,0 +1,13 @@
+package pcy.study.tobyspring6.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record ExchangeRateData(
+        String result,
+        Map<String, BigDecimal> rates
+) {
+}
---
+++ toby-spring-6/src/main/java/pcy/study/tobyspring6/domain/Payment.java
@@ -0,0 +1,28 @@
+package pcy.study.tobyspring6.domain;
+
+import lombok.Getter;
+import lombok.ToString;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@ToString
+public class Payment {
+
+    private final Long orderId;
+    private final String currency;
+    private final BigDecimal foreignCurrencyAmount;
+    private final BigDecimal exchangeRate;
+    private final BigDecimal convertedAmount;
+    private final LocalDateTime validUntil;
+
+    public Payment(Long orderId, String currency, BigDecimal foreignCurrencyAmount, BigDecimal exchangeRate, BigDecimal convertedAmount, LocalDateTime validUntil) {
+        this.orderId = orderId;
+        this.currency = currency;
+        this.foreignCurrencyAmount = foreignCurrencyAmount;
+        this.exchangeRate = exchangeRate;
+        this.convertedAmount = convertedAmount;
+        this.validUntil = validUntil;
+    }
+}
