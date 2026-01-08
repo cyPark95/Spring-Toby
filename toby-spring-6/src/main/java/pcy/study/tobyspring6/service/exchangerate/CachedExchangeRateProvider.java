675a3e731845bf1ff8f6863919a465d5225091b7
cyPark95 <parkcy1007@gmail.com>
2026-01-08 11:48:40 +0900

    feat: 스프링6 강의 섹션 3.관심사 분리 - Decorator 디자인 패턴 도입

---
+++ toby-spring-6/src/main/java/pcy/study/tobyspring6/service/exchangerate/CachedExchangeRateProvider.java
@@ -0,0 +1,36 @@
+package pcy.study.tobyspring6.service.exchangerate;
+
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CachedExchangeRateProvider implements ExchangeRateProvider {
+
+    private final ExchangeRateProvider target;
+
+    private final Map<String, BigDecimal> cachedExchangeRates = new HashMap<>();
+    private final Map<String, LocalDateTime> cacheExpiryTimes = new HashMap<>();
+
+    public CachedExchangeRateProvider(ExchangeRateProvider target) {
+        this.target = target;
+    }
+
+    @Override
+    public BigDecimal getExchangeRate(String currency) throws IOException {
+        if(isValidCache(currency)) {
+            return cachedExchangeRates.get(currency);
+        }
+
+        BigDecimal exchangeRate = target.getExchangeRate(currency);
+        cachedExchangeRates.put(currency, exchangeRate);
+        cacheExpiryTimes.put(currency, LocalDateTime.now().plusSeconds(3L));
+        return exchangeRate;
+    }
+
+    private boolean isValidCache(String currency) {
+        return cachedExchangeRates.containsKey(currency) && cacheExpiryTimes.get(currency).isAfter(LocalDateTime.now());
+    }
+}
