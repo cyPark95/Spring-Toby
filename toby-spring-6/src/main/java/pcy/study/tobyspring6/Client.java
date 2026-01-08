75336aea9525783671099d4ed39896ea404a0b41
cyPark95 <parkcy1007@gmail.com>
2026-01-08 11:35:16 +0900

    feat: 스프링6 강의 섹션 3.관심사 분리 - 스프링 IoC/DI

--- toby-spring-6/src/main/java/pcy/study/tobyspring6/Client.java
+++ toby-spring-6/src/main/java/pcy/study/tobyspring6/Client.java
@@ -1,5 +1,7 @@
 package pcy.study.tobyspring6;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import pcy.study.tobyspring6.domain.Payment;
 import pcy.study.tobyspring6.service.PaymentService;
 
@@ -8,8 +10,8 @@ import java.math.BigDecimal;
 public class Client {
 
     public static void main(String[] args) throws Exception {
-        ObjectFactory factory = new ObjectFactory();
-        PaymentService paymentService = factory.paymentService();
+        BeanFactory factory = new AnnotationConfigApplicationContext(ObjectFactory.class);
+        PaymentService paymentService = factory.getBean(PaymentService.class);
         Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
         System.out.println(payment);
     }
--- toby-spring-6/src/main/java/pcy/study/tobyspring6/ObjectFactory.java
+++ toby-spring-6/src/main/java/pcy/study/tobyspring6/ObjectFactory.java
@@ -1,15 +1,20 @@
 package pcy.study.tobyspring6;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import pcy.study.tobyspring6.service.PaymentService;
 import pcy.study.tobyspring6.service.exchangerate.ExchangeRateProvider;
 import pcy.study.tobyspring6.service.exchangerate.WebApiExchangeRateProvider;
 
+@Configuration
 public class ObjectFactory {
 
+    @Bean
     public PaymentService paymentService() {
         return new PaymentService(exchangeRateProvider());
     }
 
+    @Bean
     public ExchangeRateProvider exchangeRateProvider() {
         return new WebApiExchangeRateProvider();
     }
