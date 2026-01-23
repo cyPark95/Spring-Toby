7e1253f7759b05d616eb2851fed1db762def65c1
cyPark95 <parkcy1007@gmail.com>
2026-01-23 14:04:33 +0900

    refactor: 스프링 부트 강의 섹션 4.서블릿 애플리케이션 - 서블릿 컨테이너 설정 & 요청 처리

--- toby-spring-boot/src/main/java/pcy/study/tobyspringboot/TobySpringBootApplication.java
+++ toby-spring-boot/src/main/java/pcy/study/tobyspringboot/TobySpringBootApplication.java
@@ -1,13 +1,34 @@
 package pcy.study.tobyspringboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
-@SpringBootApplication
+import java.io.IOException;
+
 public class TobySpringBootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TobySpringBootApplication.class, args);
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = serverFactory.getWebServer(servletContext -> servletContext.addServlet(
+                "hello",
+                new HttpServlet() {
+                    @Override
+                    protected void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
+                        String name = request.getParameter("name");
+
+                        response.setStatus(HttpStatus.OK.value());
+                        response.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                        response.getWriter().print("Hello " + name);
+                    }
+                }).addMapping("/hello"));
+        webServer.start();
     }
 
 }
