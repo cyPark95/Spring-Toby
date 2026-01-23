8631250ee1943a0dd775ef021ef522a00308f711
cyPark95 <parkcy1007@gmail.com>
2026-01-23 14:12:04 +0900

    feat: 스프링 부트 강의 섹션 4.서블릿 애플리케이션 - Front Controller 도입

--- toby-spring-boot/src/main/java/pcy/study/tobyspringboot/TobySpringBootApplication.java
+++ toby-spring-boot/src/main/java/pcy/study/tobyspringboot/TobySpringBootApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -16,18 +17,33 @@ public class TobySpringBootApplication {
 
     public static void main(String[] args) {
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = serverFactory.getWebServer(servletContext -> servletContext.addServlet(
-                "hello",
-                new HttpServlet() {
-                    @Override
-                    protected void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
-                        String name = request.getParameter("name");
+        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            HelloController helloController = new HelloController();
 
-                        response.setStatus(HttpStatus.OK.value());
-                        response.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-                        response.getWriter().print("Hello " + name);
-                    }
-                }).addMapping("/hello"));
+            servletContext.addServlet(
+                    "front-controller",
+                    new HttpServlet() {
+                        @Override
+                        protected void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
+                            // 인증, 보안, 다국어, 공통 기능 처리
+                            // ...
+
+                            if (request.getRequestURI().equals("/hello") && request.getMethod().equals(HttpMethod.GET.name())) {
+                                String name = request.getParameter("name");
+
+                                String message = helloController.hello(name);
+
+                                response.setStatus(HttpStatus.OK.value());
+                                response.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                                response.getWriter().print(message);
+                            } else if (request.getRequestURI().equals("/user")) {
+                                //
+                            } else {
+                                response.setStatus(HttpStatus.NOT_FOUND.value());
+                            }
+                        }
+                    }).addMapping("/*");
+        });
         webServer.start();
     }
 
