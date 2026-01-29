935ecb409b89d5bfc763a5b34c4e687d9794729c
cyPark95 <parkcy1007@gmail.com>
2026-01-29 13:32:55 +0900

    refactor: 스프링 부트 강의 섹션 5.스프링 애플리케이션 - SpringBoot Application

---
+++ toby-spring-boot/src/main/java/pcy/study/tobyspringboot/SpringApplication.java
@@ -0,0 +1,29 @@
+package pcy.study.tobyspringboot;
+
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class SpringApplication {
+
+    public static void run(Class<?> applicationClass, String... args) {
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
+            @Override
+            protected void onRefresh() {
+                super.onRefresh();
+
+                ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
+                DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
+                dispatcherServlet.setApplicationContext(this);
+
+                WebServer webServer = serverFactory.getWebServer(servletContext -> servletContext
+                        .addServlet("dispatcher-servlet", dispatcherServlet)
+                        .addMapping("/*"));
+                webServer.start();
+            }
+        };
+        applicationContext.register(applicationClass);
+        applicationContext.refresh();
+    }
+}
--- toby-spring-boot/src/main/java/pcy/study/tobyspringboot/TobySpringBootApplication.java
+++ toby-spring-boot/src/main/java/pcy/study/tobyspringboot/TobySpringBootApplication.java
@@ -1,33 +1,27 @@
 package pcy.study.tobyspringboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @Configuration
 @ComponentScan
 public class TobySpringBootApplication {
 
-    public static void main(String[] args) {
-        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
-            @Override
-            protected void onRefresh() {
-                super.onRefresh();
-
-                ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-                WebServer webServer = serverFactory.getWebServer(servletContext -> servletContext.addServlet(
-                        "dispatcher-servlet",
-                        new DispatcherServlet(this)
-                ).addMapping("/*"));
-                webServer.start();
-            }
-        };
-        applicationContext.register(TobySpringBootApplication.class);
-        applicationContext.refresh();
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+        return new TomcatServletWebServerFactory();
     }
 
+    @Bean
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(TobySpringBootApplication.class, args);
+    }
 }
