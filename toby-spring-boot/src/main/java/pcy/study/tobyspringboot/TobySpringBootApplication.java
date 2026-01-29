8e0f513cedebe8e1d5ced6421a5dcb045f9ee3c1
cyPark95 <parkcy1007@gmail.com>
2026-01-29 13:06:47 +0900

    refactor: 스프링 부트 강의 섹션 5.스프링 애플리케이션 - 스프링 컨테이너 통합 & 자바 코드 구성 정보 도입

--- toby-spring-boot/src/main/java/pcy/study/tobyspringboot/TobySpringBootApplication.java
+++ toby-spring-boot/src/main/java/pcy/study/tobyspringboot/TobySpringBootApplication.java
@@ -3,25 +3,43 @@ package pcy.study.tobyspringboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 import pcy.study.tobyspringboot.hello.controller.HelloController;
+import pcy.study.tobyspringboot.hello.service.HelloService;
 import pcy.study.tobyspringboot.hello.service.SimpleHelloService;
 
+@Configuration
 public class TobySpringBootApplication {
 
-    public static void main(String[] args) {
-        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
-        applicationContext.registerBean(HelloController.class);
-        applicationContext.registerBean(SimpleHelloService.class);
-        applicationContext.refresh();
+    @Bean
+    public HelloController helloController(HelloService helloService) {
+        return new HelloController(helloService);
+    }
 
-        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = serverFactory.getWebServer(servletContext -> servletContext.addServlet(
-                "dispatcher-servlet",
-                new DispatcherServlet(applicationContext)
-        ).addMapping("/*"));
-        webServer.start();
+    @Bean
+    public HelloService helloService() {
+        return new SimpleHelloService();
+    }
+
+    public static void main(String[] args) {
+        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
+            @Override
+            protected void onRefresh() {
+                super.onRefresh();
+
+                ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+                WebServer webServer = serverFactory.getWebServer(servletContext -> servletContext.addServlet(
+                        "dispatcher-servlet",
+                        new DispatcherServlet(this)
+                ).addMapping("/*"));
+                webServer.start();
+            }
+        };
+        applicationContext.register(TobySpringBootApplication.class);
+        applicationContext.refresh();
     }
 
 }
