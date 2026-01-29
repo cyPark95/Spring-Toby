430b917485e5ea01cf14fddefa61e026a66a685a
cyPark95 <parkcy1007@gmail.com>
2026-01-29 12:54:14 +0900

    refactor: 스프링 부트 강의 섹션 5.스프링 애플리케이션 - DispatcherServlet 전환

--- toby-spring-boot/src/main/java/pcy/study/tobyspringboot/TobySpringBootApplication.java
+++ toby-spring-boot/src/main/java/pcy/study/tobyspringboot/TobySpringBootApplication.java
@@ -1,50 +1,26 @@
 package pcy.study.tobyspringboot;
 
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 import pcy.study.tobyspringboot.hello.controller.HelloController;
 import pcy.study.tobyspringboot.hello.service.SimpleHelloService;
-
-import java.io.IOException;
 
 public class TobySpringBootApplication {
 
     public static void main(String[] args) {
-        GenericApplicationContext applicationContext = new GenericApplicationContext();
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
         applicationContext.registerBean(HelloController.class);
         applicationContext.registerBean(SimpleHelloService.class);
         applicationContext.refresh();
 
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext -> servletContext.addServlet(
-                "front-controller",
-                new HttpServlet() {
-                    @Override
-                    protected void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
-                        // 인증, 보안, 다국어, 공통 기능 처리
-                        // ...
-
-                        if (request.getRequestURI().equals("/hello") && request.getMethod().equals(HttpMethod.GET.name())) {
-                            String name = request.getParameter("name");
-
-                            HelloController helloController = applicationContext.getBean(HelloController.class);
-                            String message = helloController.hello(name);
-
-                            response.setContentType(MediaType.TEXT_PLAIN_VALUE);
-                            response.getWriter().print(message);
-                        } else {
-                            response.setStatus(HttpStatus.NOT_FOUND.value());
-                        }
-                    }
-                }).addMapping("/*"));
+                "dispatcher-servlet",
+                new DispatcherServlet(applicationContext)
+        ).addMapping("/*"));
         webServer.start();
     }
 
--- toby-spring-boot/src/main/java/pcy/study/tobyspringboot/hello/controller/HelloController.java
+++ toby-spring-boot/src/main/java/pcy/study/tobyspringboot/hello/controller/HelloController.java
@@ -1,6 +1,8 @@
 package pcy.study.tobyspringboot.hello.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import pcy.study.tobyspringboot.hello.service.HelloService;
 
 import java.util.Objects;
@@ -10,6 +12,8 @@ public class HelloController {
 
     private final HelloService helloService;
 
+    @GetMapping("/hello")
+    @ResponseBody
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
